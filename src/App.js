582308da55cdb99ce35a3829938a263b1f8692ff
Peter Simmons <peterorpete@me.com>
2020-01-26 23:00:25 +0000

    second react lesson complete

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const welcome = "Welcome to React";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Welcome></Welcome>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -22,5 +25,11 @@ function App() {
     </div>
   );
 }
+
+function Welcome()  {
+    return (
+      <h1 className="App-title">{welcome}</h1>
+    )
+};
 
 export default App;
